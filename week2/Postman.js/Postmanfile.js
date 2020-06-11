d7307b2c75b16641d3080ccd002d2d37e4ce1039
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-06-10 19:01:28 -0700

    Finding & Testing APIs In Postman Assignment

---
+++ week2/Postman.js/Postmanfile.js
@@ -0,0 +1,108 @@
+var results = `{
+    "response_code": 0,
+    "results": [
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "The%20retail%20disc%20of%20Tony%20Hawk%27s%20Pro%20Skater%205%20only%20comes%20with%20the%20tutorial.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "In%20%22The%20Sims%22%20series%2C%20the%20most%20members%20in%20a%20household%20you%20can%20have%20is%208.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "In%20the%20game%20%22Melty%20Blood%20Actress%20Again%20Current%20Code%22%2C%20you%20can%20enter%20Blood%20Heat%20mode%20in%20Half%20Moon%20style.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "The%20first%20%22Metal%20Gear%22%20game%20was%20released%20for%20the%20PlayStation%201.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "In%20%22Portal%202%22%2C%20Cave%20Johnson%20started%20out%20Aperture%20Science%20as%20a%20shower%20curtain%20company.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "In%20%22Metal%20Gear%20Solid%202%22%2C%20you%20will%20see%20through%20the%20eyes%20of%20Psycho%20Mantis%20if%20you%20go%20first%20person%20during%20his%20boss%20fight.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "TF2%3A%20Sentry%20rocket%20damage%20falloff%20is%20calculated%20based%20on%20the%20distance%20between%20the%20sentry%20and%20the%20enemy%2C%20not%20the%20engineer%20and%20the%20enemy",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "The%20names%20of%20Roxas%27s%20Keyblades%20in%20Kingdom%20Hearts%20are%20%22Oathkeeper%22%20and%20%22Oblivion%22.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "All%20of%20these%20maps%20were%20in%20%22Tom%20Clancy%27s%20Rainbow%20Six%20Siege%22%20on%20its%20initial%20release%3A%20House%2C%20Clubhouse%2C%20Border%2C%20Consulate.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Entertainment%3A%20Video%20Games",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "The%20video%20game%20%22Fuel%22%20has%20an%20open%20world%20that%20is%205%2C560%20square%20miles%3F",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        }
+    ]
+}`
+var obj = document.getElementById("elements")
+//console.log(results)//
+obj.innerHTML = results
