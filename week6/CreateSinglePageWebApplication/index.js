f45ac496d5723f15f592d7ce1ccb81d2eb51d5cf
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-07-12 18:10:02 -0700

    Create A Single Web Application Assignment

---
+++ week6/CreateSinglePageWebApplication/index.js
@@ -0,0 +1,36 @@
+async function getData(){
+    
+    try {
+        const response = await axios.get("https://rickandmortyapi.com/api/location/4") 
+        
+        listLocationsToDOM(response)
+    
+    for (let i = 0 ; i < 2; i++){ 
+         
+        console.log(response.data.residents[i])
+        console.log(response.data.residents)
+         const locations = await axios.get(response.data.residents[i])
+         console.log(locations)
+         listLocationsToDOM(locations)
+    }
+
+        
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+    
+}
+
+getData()
+
+function listLocationsToDOM(locations){
+    const h1 = document.createElement("h1")
+    h1.textContent = `${locations.data.name}`
+    document.body.appendChild(h1)
+}
+
+// axios.get("https://rickandmortyapi.com/api/location/4").then(res=>{
+//     console.log(res)
+// })
